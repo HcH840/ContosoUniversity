685cfd2dd4c6d2d6b0b3dfe4a0545c76a4d3676c
WorkGroup-2014 <WorkGroup-2014@WORKGR-2016>
2023-08-02 01:56:43 -0800

    Add project files.

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ContosoUniversity.Models
+{ 
+public class Student
+	{
+		public int ID { get; set; }
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+        public Student()
+			{
+
+			}
+	}
+}
